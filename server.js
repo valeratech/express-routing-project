5c440304caaf222c170b0fb252b645739c0bbe9c
Ryan Valera <valeraryan@gmail.com>
2023-11-14 05:28:24 -0800

    Add middleware to project to handle different tasks of the application.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-const http = require('http');
+// const http = require('http');
 const express = require('express');
 
+// https://github.com/expressjs/express/tree/master/lib
 // Initialize express object and pass it in the createServer method as a valid handler
 const app = express();
 
@@ -9,13 +10,24 @@ const app = express();
 // and allows us to add a new middleware function
 app.use((req, res, next) => {
     console.log('In the first middleware');
-    next(); // Allows the request to continue to the next middleware in the line
+
+    // Allows the request to continue to the next middleware in the line.
+    // If a response is sent in this current middleware, the next middleware doesn't fire off
+    next();
 });
 
 app.use((req, res, next) => {
     console.log('In the second middleware');
+
+    // Express doesn't send a default response so you must explicitly state it
+    // res.setHeader() <<< This is possible in Express
+    // res.setHeader() <<< This is also possible in Express
+    res.send() // Automatically sets a default Content-Type if not specified
 })
 
-const server = http.createServer(app);
 
-server.listen(3000);
+// This method does the same as the 2 lines below it
+app.listen(3000);
+
+// const server = http.createServer(app);
+// server.listen(3000);
