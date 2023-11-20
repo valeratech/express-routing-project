7e2ea8e2566a858deb76f98179a5d16f4c23c558
Ryan Valera <valeraryan@gmail.com>
2023-11-20 13:29:25 -0800

    Create shop.ejs template/view with a conditional html block to render items that were pushed and added through the form.

--- routes/shop.js
+++ routes/shop.js
@@ -6,13 +6,14 @@ const adminRoutes = require('./admin');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    console.log(adminRoutes.products);
+    const products = adminRoutes.products
     // Express doesn't send a default response so you must explicitly state it
     // res.setHeader() <<< This is possible in Express
     // res.write() <<< This is also possible in Express
     // res.sendFile(path.join(rootDir, 'views', 'shop.html')); // Automatically sets a default Content-Type if not specified
     res.render('shop', {
         pageTitle: 'Shop',
+        products: products
     });
 })
 
